6f3e6c5c60013414713b350519e889ca520d9dd5
haoxiaoli <haoxiaoli@baidu.com>
2021-01-13 20:10:37 +0800

    submit

--- src/index/Submit.jsx
+++ src/index/Submit.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './Submit.css';
-export default function Submit(props) {
-    return <div></div>;
-}
+
+// 无任何参数传递，故可用memo
+export default memo(function Submit(props) {
+    return (
+        <div className="submit">
+            <button type="submit" className="submit-button">
+                搜索
+            </button>
+        </div>
+    );
+});
