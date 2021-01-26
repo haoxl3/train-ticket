2f62afe1f4022c52241b8d12458d645528d8a2be
haoxiaoli <haoxiaoli@baidu.com>
2021-01-26 18:23:57 +0800

    Detail

--- src/ticket/App.jsx
+++ src/ticket/App.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import URI from 'urijs';
+import dayjs from 'dayjs';
+import { h0 } from '../common/fp';
+import Header from '../common/Header';
+import Detail from '../common/Detail';
+import Candidate from './Candidate';
+import Schedule from './Schedule';
+import Nav from '../common/Nav';
+import useNav from '../common/useNav';
 import './App.css';
+
+import {
+    setDepartStation,
+    setArriveStation,
+    setTrainNumber,
+    setDepartDate,
+    setSearchParsed,
+    prevDate,
+    nextDate,
+    setDepartTimeStr,
+    setArriveTimeStr,
+    setArriveDate,
+    setDurationStr,
+    setTickets,
+    toggleIsScheduleVisible,
+} from './actions';
 
 function App(props) {
     const {
@@ -17,7 +43,98 @@ function App(props) {
         isScheduleVisible,
         searchParsed,
     } = props;
-    return <div className="app"></div>;
+
+    const onBack = useCallback(() => {
+        window.history.back();
+    }, []);
+    useEffect(() => {
+        // 获取URL参数后并设置store中的值
+        const queries = URI.parseQuery(window.location.search);
+        const { aStation, dStation, date, trainNumber } = queries;
+        dispatch(setDepartStation(dStation));
+        dispatch(setArriveStation(aStation));
+        dispatch(setTrainNumber(trainNumber));
+        dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+
+        dispatch(setSearchParsed(true));
+    }, []);
+    useEffect(() => {
+        document.title = trainNumber;
+    }, [trainNumber]);
+    useEffect(() => {
+        if (!searchParsed) {
+            return;
+        }
+        const url = new URI('/rest/ticket')
+            .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+            .setSearch('trainNumber', trainNumber)
+            .toString();
+        fetch(url)
+            .then(res => res.json())
+            .then(result => {
+                const { detail, candidates } = result;
+                const {
+                    departTimeStr,
+                    arriveTimeStr,
+                    arriveDate,
+                    durationStr,
+                } = detail;
+                dispatch(setDepartTimeStr(departTimeStr));
+                dispatch(setArriveTimeStr(arriveTimeStr));
+                dispatch(setArriveDate(arriveDate));
+                dispatch(setDurationStr(durationStr));
+                dispatch(setTickets(candidates));
+            });
+    }, [searchParsed, departDate, trainNumber]);
+    const { isPrevDisabled, isNextDisabled, prev, next } = useNav(
+        departDate,
+        dispatch,
+        prevDate,
+        nextDate
+    );
+
+    const detailCbs = useMemo(() => {
+        return bindActionCreators(
+            {
+                toggleIsScheduleVisible,
+            },
+            dispatch
+        );
+    }, []);
+
+    // 如果URL无参则返回空的HTML结构
+    if (!searchParsed) {
+        return null;
+    }
+    return (
+        <div className="app">
+            <div className="header-wrapper">
+                <Header title={trainNumber} onBack={onBack} />
+            </div>
+            <div className="nav-wrapper">
+                <Nav
+                    date={departDate}
+                    isPrevDisabled={isPrevDisabled}
+                    isNextDisabled={isNextDisabled}
+                    prev={prev}
+                    next={next}
+                />
+            </div>
+            <div className="detail-wrapper">
+                <Detail
+                    departDate={departDate}
+                    arriveDate={arriveDate}
+                    departTimeStr={departTimeStr}
+                    arriveTimeStr={arriveTimeStr}
+                    trainNumber={trainNumber}
+                    departStation={departStation}
+                    arriveStation={arriveStation}
+                    durationStr={durationStr}
+                    {...detailCbs}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default connect(
---
+++ src/ticket/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import * as serviceWorker from '../serviceWorker';
+
+import store from './store';
+import './index.css';
+import App from './App.jsx';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
